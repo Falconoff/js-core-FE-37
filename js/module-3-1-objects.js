d20edb5eaa81beaa60a9f055d52572e72e672374
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-08-01 23:26:52 +0300

    fix conflict

--- js/module-3-1-objects.js
+++ js/module-3-1-objects.js
@@ -50,7 +50,9 @@
 //   console.log(`${key}: ${value}`)
 // }
 
+
 // ================= Распыление аргументов =================
+
 // const temperatures = [18, 14, 12, 21, 17, 29];
 
 // const min = Math.min(...temperatures); // NaN
@@ -58,63 +60,113 @@
 // console.log(...temperatures)  // 18 14 12 21 17 29
 // console.log(min)   // 12
 
+
+// ============= Распыление массивов ===================
+// const houses = ['Arryn', 'Frey', 'Greyjoy', 'Stark', 'Lannister', 'Tyrell', 'Targaryen', 'Arryn', 'Frey', 'Greyjoy', 'Stark', 'Lannister', 'Tyrell'];
+// console.table(houses)
+
+// let withoutStark = [...houses];
+
+// for (let i = 0; i <= withoutStark.length; i+=1) {
+//   if (withoutStark[i] === 'Stark') {
+//     withoutStark.splice(i, 1)
+//   }
+// }
+// console.table(withoutStark);
+
+
+
+// const arr1 = ['dd', 'ee']
+// const arr2 = ['aa', 'bb', 'cc']
+// const arrTotal = [...arr2, ...arr1]
+// console.log(arrTotal)
+
+
+// const houses = ['Arryn', 'Frey', 'Greyjoy', 'Stark', 'Lannister', 'Tyrell', 'qweqwe', 'ewqewq'];
+// console.table(houses);
+// // Вертикальная запись удобнее для чтения
+// const updatedHouses = [
+//   ...houses.slice(0, 1),
+//   'Frey of the Crossing',
+//   ...houses.slice(2, 4),
+//   'Lannister canister',
+//   ...houses.slice(5)
+// ];
+// console.table(updatedHouses);
+// // ['Arryn', 'Frey of the Crossing', 'Greyjoy', 'Stark', 'Lannister', 'Tyrell']
+
+
+
 // =============== ШКОЛЬНЫЙ ТАБЕЛЬ И СРЕДНЯЯ ОЦЕНКА ===============
-const anna = {
-  name: 'Anna Ivanova',
-  marks: {
-    math: 10,
-    ukrLang: 11,
-    ukrLit: 10,
-    engl: 9,
-    history: 10,
-    geogr: 11,
-    art: 10,
-    music: 10,
-  },
-}
+// const anna = {
+//   name: 'Anna Ivanova',
+//   marks: {
+//     math: 10,
+//     ukrLang: 11,
+//     ukrLit: 10,
+//     engl: 9,
+//     history: 10,
+//     geogr: 11,
+//     art: 10,
+//     music: 10,
+//   },
+// }
 
-const peter = {
-  name: 'Peter Sidoroff',
-  marks: {
-    math: 7,
-    ukrLang: 8,
-    ukrLit: 8,
-    engl: 6,
-    history: 5,
-    geogr: 8,
-    art: 12,
-  },
-  getAvgMark() {
-    let avgMark = 0
-    let total = 0
+// const peter = {
+//   name: 'Peter Sidoroff',
+//   marks: {
+//     math: 7,
+//     ukrLang: 8,
+//     ukrLit: 8,
+//     engl: 6,
+//     history: 5,
+//     geogr: 8,
+//     art: 12,
+//   },
+//   getAvgMark() {
+//     let avgMark = 0
+//     let total = 0
 
-    const marks = Object.values(this.marks)
-    console.log(`${this.name}'s marks array: ${marks}`)
+//     const marks = Object.values(this.marks)
+//     console.log(`${this.name}'s marks array: ${marks}`)
 
-    for (const mark of marks) {
-      total += mark
-    }
+//     for (const mark of marks) {
+//       total += mark
+//     }
 
-    avgMark = (total / marks.length).toFixed(1)
-    console.log(`avgMark: ${avgMark}`)
-  },
-}
+//     avgMark = (total / marks.length).toFixed(1)
+//     console.log(`avgMark: ${avgMark}`)
+//   },
+// }
 
-function getAvgMark(name) {
-  let avgMark = 0
-  let total = 0
+// function getAvgMark(name) {
+//   let avgMark = 0
+//   let total = 0
 
-  const marks = Object.values(name.marks)
-  console.log(`${name.name}'s marks array: ${marks}`)
+//   const marks = Object.values(name.marks)
+//   console.log(`${name.name}'s marks array: ${marks}`)
 
-  for (const mark of marks) {
-    total += mark
-  }
+//   for (const mark of marks) {
+//     total += mark
+//   }
 
-  avgMark = (total / marks.length).toFixed(1)
-  console.log(`avgMark: ${avgMark}`)
-}
+//   avgMark = (total / marks.length).toFixed(1)
+//   console.log(`avgMark: ${avgMark}`)
+// }
 
-getAvgMark(anna)
-// getAvgMark(peter)
-peter.getAvgMark()
+// getAvgMark(anna)
+// // getAvgMark(peter)
+// peter.getAvgMark()
+
+
+const animal = {
+  legs: 4,
+};
+const dog = Object.create(animal);
+console.log(dog); // {name: 'Манго'}
+
+// dog.name = "Манго";
+
+// console.log(dog); // {name: 'Манго'}
+// console.log(dog.name); // 'Манго'
+// console.log(dog.legs); // 4
