9a8771c439859bf7c3ffc0f47b0a5cf029d26414
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-08-02 23:29:23 +0300

    Update hw-03.js

--- js/hw-03.js
+++ js/hw-03.js
@@ -91,29 +91,136 @@
 // getAllPropValues('prices')
 
 // =============== 20 ================
-const products = [
-  { name: 'Radar', price: 1300, quantity: 4 },
-  { name: 'Scanner', price: 2700, quantity: 3 },
-  { name: 'Droid', price: 400, quantity: 7 },
-  { name: 'Grip', price: 1200, quantity: 9 },
-]
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ]
 
-function calculateTotalPrice(productName) {
-  // Пиши код ниже этой строки
-  for (const product of products) {
-    if (product.name === productName) {
-      console.log(
-        `I find ${productName}! It's  price: ${
-          product.price * product.quantity
-        }`,
-      )
+// function calculateTotalPrice(productName) {
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       console.log(
+//         `I find ${productName}! It's  price: ${
+//           product.price * product.quantity
+//         }`,
+//       )
+//       return product.price * product.quantity
+//     }
+//   }
+//   console.log(`I DON'T find ${productName}`)
+//   return 0
+// }
+// calculateTotalPrice('Radar')
 
-      return product.price * product.quantity
+
+// ====== 31 =======
+// function add(...args) {
+//   let total = 0;
+//   console.log(args);
+//   for (let arg of args) {
+//     total += arg
+//   }
+//   console.log(total);
+
+//   return total;
+// }
+
+// add(15, 27)
+// add(12, 4, 11, 48)
+
+// ======== 33 =========
+// function findMatches(array, ...numbers) {
+//   const matches = []; // Don't change this line
+//   for(let num of numbers) {
+//     if (array.includes(num)) {
+//         console.log(`match ${num}`);
+//         matches.push(num)
+//     }
+//   }
+//   console.log(matches);
+//   return matches;
+// }
+// findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41)
+
+// ======== 35 =============
+// const bookShelf = {
+//   books: ['The last kingdom', 'Haze', 'The guardian of dreams'],
+//   updateBook(oldName, newName) {
+//     // находим индекс старой книги
+//     console.log(this.books.indexOf(oldName));
+//     const bookIndex = this.books.indexOf(oldName);
+//     // заменяем старую на новую
+//     this.books.splice(bookIndex, 1, newName)
+//   },
+// };
+// bookShelf.updateBook("Haze", "Dungeon chronicles")
+
+
+// ========== 41 ==============
+/*
+getPotions() - метод для получения всех зелий. Возвращает значение свойства potions.
+addPotion(newPotion) - добавляет зелье newPotion (уже объект) в массив в свойстве potions.
+removePotion(potionName) - удаляет объект зелья с именем potionName из массива в свойстве potions.
+updatePotionName(oldName, newName) - обновляет свойство name объекта-зелья с названием oldName на newName в массиве potions.
+*/
+const atTheOldToad = {
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    console.log(this.potions)
+    return this.potions;
+  },
+
+  addPotion(newPotion) {
+    // console.log(this.potions)
+    for (const potion of this.potions) {
+      if (potion.name === newPotion.name) {
+        console.log(`Potion ${newPotion.name} is already equipped!`);
+        return `Potion ${newPotion.name} is already equipped!`;
+      }
     }
-  }
-  console.log(`I DON'T find ${productName}`)
-  return 0
-}
-// Пиши код выше этой строки
+    this.potions.push(newPotion);
+    // console.log(this.potions);
+  },
 
-calculateTotalPrice('Radar')
+  removePotion(potionName) {
+    const { potions } = this;
+    for (let i = 0; i < potions.length; i += 1) {
+      const potion = potions[i];
+      if (potion.name === potionName) {
+        potions.splice(i, 1);
+        return
+      }
+    }
+    return `Potion ${potionName} is not in inventory!`;
+  },
+
+  updatePotionName(oldName, newName) {
+    const { potions } = this;
+    for (let i = 0; i < potions.length; i += 1) {
+      const potion = potions[i];
+      if (potion.name === oldName) {
+        potion.name = newName
+        return
+      }
+    }
+    return `Potion ${oldName} is not in inventory!`;
+  },
+  // Change code above this line
+};
+
+console.table(atTheOldToad.potions)
+// atTheOldToad.getPotions()
+// atTheOldToad.addPotion({ name: "Invisibility", price: 620 })
+// atTheOldToad.addPotion({ name: "Power potion", price: 270 })
+// atTheOldToad.removePotion("Dragon breath")
+// atTheOldToad.removePotion("Speed potion")
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth")
+atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion")
+console.table(atTheOldToad.potions)
