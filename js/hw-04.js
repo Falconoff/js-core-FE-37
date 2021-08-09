de60163556ed24a3f0903eb5d67f152f94a8e413
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-08-09 17:08:03 +0300

    Update hw-04.js

--- js/hw-04.js
+++ js/hw-04.js
@@ -1,4 +1,4 @@
-// 4 - callback
+// 4 - callback ---------------------------------------
 /*
 const pizzaPalace = {
   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
@@ -32,8 +32,8 @@ pizzaPalace.order('Биг майк', makePizza, onOrderError)
 pizzaPalace.order('Венская', makePizza, onOrderError)
 */
 
-// 7 - call
-
+// 7 - call ---------------------------------------
+/*
 const orders = [
   { email: 'solomon@topmail.ua', dish: 'Burger' },
   { email: 'artemis@coldmail.net', dish: 'Pizza' },
@@ -45,7 +45,7 @@ const orders = [
 // Пиши код ниже этой строки
 function composeMessage(position) {
   console.log('composeMessage called');
-  return (`Готовим ${this.dish} для ${this.email}. Ваш заказ ${position + 1}-й в очереди.`);
+  return (`Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`);
   // console.log('messages', messages);
   
 }
@@ -53,10 +53,72 @@ function composeMessage(position) {
 const messages = [];
 
 for(let i = 0; i <orders.length; i +=1) {
-  messages.push(composeMessage.call(orders[i], i));
+  messages.push(composeMessage.call(orders[i], i + 1));
   console.log('i=', i);
 }
 
 // composeMessage.call(order, 0);
 
 console.log('messages', messages);
+*/
+
+// 10 ---------------------------------------
+
+// an example from the Theory
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() это вызов метода getFullName без объекта
+//   console.log(`Обрабатываем заявку от ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName); // Будет ошибка при вызове функции:
+/* Uncaught TypeError: Cannot read property 'firstName' of undefined
+    at getFullName (hw-04.js:71)
+    at makeMessage (hw-04.js:77)
+    at hw-04.js:80
+*/
+// task ---------------------------------------
+const service = {
+  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+  subscribe(email) {
+    this.mailingList.push(email);
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    this.mailingList = this.mailingList.filter(e => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return action(email);
+}
+
+const firstInvoke = logAndInvokeAction(
+  'kiwi@mail.uk',
+  service.subscribe.bind(service),
+);
+console.log(firstInvoke);
+// Почта kiwi@mail.uk добавлена в рассылку.
+
+console.log(service.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+const secondInvoke = logAndInvokeAction(
+  'poly@hotmail.de',
+  service.unsubscribe.bind(service),
+);
+console.log(secondInvoke);
+// Почта poly@hotmail.de удалена из рассылки.
+
+console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
