0ad85e87475b8f35441a248358e6ebc0dc583473
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-08-16 22:52:56 +0300

    Update hw-05.js

--- js/hw-05.js
+++ js/hw-05.js
@@ -275,3 +275,108 @@ const bmw = new Car({ price: 64000 });
 console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
 console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
 */
+
+// 18 ================================
+/*
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  // Пиши код ниже этой строки
+  accessLevel;
+
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
+  constructor({email, accessLevel}) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+*/
+
+// 19 ========================================
+/*
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Пиши код ниже этой строки
+
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
+
+  accessLevel;
+  blacklistedEmails = [];
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+  
+  blacklist(email) {
+    this.blacklistedEmails.push(email)
+  }
+  
+  isBlacklisted(email) {
+    // if(this.blacklistedEmails.includes(email)) {
+    //   return true
+    // } else {
+    //   return false
+    // }
+    return this.blacklistedEmails.includes(email);
+  }
+
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted('mango@mail.com')); //  false
+console.log(mango.isBlacklisted('poly@mail.com')); // true 
+*/
